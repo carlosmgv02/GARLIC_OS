cdbf268287f408468e1ab4f0d9439735aa5c84bd
Genis Martinez <genis.martinez@estudiants.urv.cat>
2023-10-20 12:24:55 +0200

    progM: inicio rutina _gm_reubicar

--- GARLIC_OS/source/garlic_itcm_mem.s
+++ GARLIC_OS/source/garlic_itcm_mem.s
@@ -23,10 +23,22 @@
 	@;Resultado:
 	@; cambio de las direcciones de memoria que se tienen que ajustar
 _gm_reubicar:
-	push {lr}
-
-
-	pop {pc}
+	push {r0-,lr}
+		ldr r11, [r0, #32]	@; e_shoff (offset de la tabla de secciones)
+		ldrh r5, [r0, #48]	@; IMPORTANTE: dudo entre #46 y #48, preguntar
+		mov r8, r0			@; muevo el buffer (primer parámetro) a r8
+		mov r9, r1			@; muevo el inicio de segmento a r9
+		mov r10, r2			@; muevo el destino de la memoria a r10
+		add r11, #4			@; incrementamos el offset
+	
+	.LBuclesecciones:
+		cmp r5, #0			@; comparo el número de entradas con contador
+		beq .LFin			@; si no hay entradas saltamos al final
+		
+	.LFin:
+	
+	
+	pop {r0-,pc}
 
 
 .end
