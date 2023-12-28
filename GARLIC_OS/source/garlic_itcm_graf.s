b6b0a16a9f4bd1301adcd3cbae3372f7f51fdd8b
Carlos Martínez <carlimvg02@gmail.com>
2023-12-29 00:07:53 +0100

    ProgG: _gg_desplazar refactored

--- GARLIC_OS/source/garlic_itcm_graf.s
+++ GARLIC_OS/source/garlic_itcm_graf.s
@@ -33,43 +33,30 @@ WBUFS_LEN = 68				@; longitud de cada buffer de ventana (64+4)
 	@;	R2: número de caracteres a escribir (int n)
 _gg_escribirLinea:
 	push {r0-r9,lr}
-		mov r3, #PPART @; r3 = PPART -> número de particiones
-		mov r4, #VFILS @; r4 = VFILS -> número de filas/ventana
-		mov r5, #PPART @; r5 = PPART -> número de particiones
-		add r5, #-1 @; r5 = PPART - 1 -> queremos trabajar con índices de 0 a PPART - 1
-		and r5, r0, r5 @; r5 = v & (PPART - 1) -> índice de la partición
-		mov r6, r0, lsr #L2_PPART @; r6 = v / L2_PPART
-		mul r7, r4, r6 @; r7 = VFILS * (v / L2_PPART) -> calculamos el desplazamiento de la ventana
-
-		mla r7, r3, r7, r5 @; 
-		mla r7, r3, r1, r7 @; 
-		mov r8, #VCOLS @; r8 = VCOLS -> número de columnas/ventana
-		mov r8, r8, lsl #1 @; r8 = VCOLS * 2 -> número de bytes/ventana
-		mul r7, r8, r7 @; r7 = VCOLS * 2 * (VFILS * (v / L2_PPART) + (v & (PPART - 1)) + f)
-
-		ldr r9, =ptrMap2
-		ldr r9, [r9] @; r9 = ptrMap2 -> puntero a la tabla de mapeo de ventanas
-		add r9, r7 @; dirección base del fondo 2 + desplazamiento de la ventana
+		mov r5, r0
+		bl _gg_calcVertice
+		mov r6, #PCOLS
+		mov r6, r6, lsl #1
+		mla r9, r6, r1, r0			@; r0 = ptrMap2 + 2 * PCOLS * f
 
 		ldr r3, =_gd_wbfs
-		mov r4, #WBUFS_LEN @; r4 = WBUFS_LEN -> longitud de cada buffer de ventana
-		mul r4, r0, r4 @; r4 = WBUFS_LEN * v -> desplazamiento del buffer de la ventana
+		mov r4, #WBUFS_LEN 			@; r4 = WBUFS_LEN -> longitud de cada buffer de ventana
+		mul r4, r5, r4 				@; r4 = WBUFS_LEN * v -> desplazamiento del buffer de la ventana
 		add r4, r3
-		add r4, #4 @; accedemos al campo "pChars"
-		mov r0, #0 @; r0 = 0 -> 
-		mov r1, #0 @; r1 = 0 -> 
-
+		add r4, #4 					@; accedemos al campo "pChars"
+		mov r0, #0 					@; r0 = 0 -> 
+		mov r1, #0 					@; r1 = 0 -> 
+		mov r2, r2, lsl #1			@;porque cada baldosa ocupa 2 bytes
+	
 	.LSigCar:
-		cmp r0, r2 @; if (r0 >= r2) goto .LEnd
+		cmp r0, r2 					@; if (r0 >= r2) goto .LEnd
 		beq .LEnd
-		ldrb r5 , [r4, r0] @; r5 = pChars[r0]
-		sub r5, #32 @; Convertimos el código ASCII a índice de la tabla de mapeo
-		mov r6, r1
-		add r6, r9
-		strh r5, [r6] @; pFondo2[r1] = r6
+		ldrh r5 , [r4, r0] 			@; r5 = pChars[r0]
 
-		add r1, #2 @; r1 += 2
-		add r0, #1 @; r0 += 1
+		strh r5, [r9, r1] 			@; pFondo2[r1] = r6
+
+		add r1, #2 					@; r1 += 2
+		add r0, #2 					@; r0 += 1
 		b .LSigCar
 	.LEnd:
 	pop {r0-r9,pc}
@@ -81,55 +68,38 @@ _gg_escribirLinea:
 	@;Parámetros:
 	@;	R0: ventana a desplazar (int v)
 _gg_desplazar:
-	push {r0-r7, lr}
-		and r1, r0, #L2_PPART @; Obtenemos el índice de la partición
-	
-		mov r2, r0, lsr #L2_PPART @; Obtenemos el índice de la ventana
-		mov r3, #VFILS @; r3 = VFILS -> número de filas/ventana
-		mul r3, r2, r3 @; r3 = VFILS * (v & (PPART - 1)) = desplazamiento de la ventana
-		mov r4, #VCOLS @; r4 = VCOLS -> número de columnas/ventana
-		mov r4, r4, lsl #1 @; r4 = VCOLS * 2 -> número de bytes/ventana
-		mov r5, #PPART @; r5 = PPART -> número de particiones
-		mla r3, r5, r3, r1 @; r3 = VCOLS * 2 * (VFILS * (v & (PPART - 1)) + (v / L2_PPART))
-
-		mul r3, r4, r3 @; r3 = VCOLS * 2 * (VFILS * (v & (PPART - 1)) + (v / L2_PPART))
-		mov r4, #PCOLS @; r4 = PCOLS -> número de columnas/pantalla
-		mov r4, r4, lsl #1 @; r4 = PCOLS * 2 -> número de bytes/pantalla
-		add r7, r3, r4 @; r3 = VCOLS * 2 * (VFILS * (v & (PPART - 1)) + (v / L2_PPART)) + PCOLS * 2
-		ldr r4, =ptrMap2
-		ldr r4, [r4] @; r3 = ptrMap2 -> puntero a la tabla de mapeo de ventanas
-
-		add r5, r4, r7 @; r5 = ptrMap2 + VCOLS * 2 * (VFILS * (v & (PPART - 1)) + (v / L2_PPART)) + PCOLS * 2
-		add r6, r4, r3 @; r6 = ptrMap2 + VCOLS * 2 * (VFILS * (v & (PPART - 1)) + (v / L2_PPART)) + PCOLS * 2 + VCOLS * 2 * (VFILS - 1)
-
-		mov r7, #PCOLS
-		mov r7, r7, lsl #1 @; r7 = PCOLS * 2 -> número de bytes/pantalla
-		mov r0, #1
-
+	push {r0-r5,lr}
+		bl _gg_calcVertice 			@; Calculate the memory address of the upper corner
+		mov r1, r0
+		mov r2, #VCOLS
+		mov r2, r2, lsl #1 			@; R2 = VCOLS * 2
+		mov r3, #PCOLS 				@; R3 = PCOLS
+		mov r3, r3, lsl #1 			@; R3 = PCOLS * 2
+		mov r4, #0 
+		mov r5, #VFILS 				@; R5 = VFILS
 	.Lscroll:
-		cmp r0, #VFILS @; if (r0 >= VFILS) goto .Lend
-		beq .Lend
-		mov r1, #0
-		add r5, r7
-		add r6, r7
+		cmp r4, r5 					@; if(r4 == r5)
+		beq .Ladjust 				@; then jump to .Ladjust
+		add r6, r1, r3 				@; R6 = PrimeraPosi + PCOLS * 2
+		bl _gs_copiaMem 			@; Call this routine to "shift" a row
+		add r4, #1 					@; r4++
+		mov r1, r6
+		b .Lscroll
+	.Ladjust:
+		sub r1, r3 					@; Handle the last row
+		mov r3, #0 
+		mov r5, #VCOLS 				@; R5 = VCOLS
+		mov r4, #0 
 	.Lcopy:
-		cmp r1, #VCOLS*2
-		addeq r0, #1
-		beq .Lscroll
-
-		ldrh r2, [r5, r1]
-		mov r3, #VFILS
-		sub r3, #1
-		cmp r0, r3
-
-		moveq r2, #0
-		strh r2, [r6, r1]
-		add r1, #2
-		b .Lcopy
-	
+		cmp r4, r5 					@; if(r4 == r5)
+		beq .Lend 					@; then jump to .Lend
+		mov r2, #0 
+		strh r2, [r1, r4] 			@; Clear the last row
+		add r3, #1 					@; r3++
+		add r4, #2 					@; r4+=2
 	.Lend:
-
-	pop {r0-r7, pc}
+		
+	pop {r0-r5,pc}
 
 	.global _gg_calcVertice
 	@; Rutina para calcular la dirección de memoria de la esquina superior, ya que lo tendremos que usar en varias funciones de las de abajo
